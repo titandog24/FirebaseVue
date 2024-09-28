7f51d023827e7807c3b3c2ae6d53d1eb324feecd
Christopher <chcoduutn@gmail.com>
2024-09-28 17:04:10 -0600

    test

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCq0SymWN5b_94EM37omlJwLbzMw6XHfb0",
-  authDomain: "fir-vue-75c97.firebaseapp.com",
-  projectId: "fir-vue-75c97",
-  storageBucket: "fir-vue-75c97.appspot.com",
-  messagingSenderId: "191158557543",
-  appId: "1:191158557543:web:5f7cff9c3e17854c5a6dc2"
+/*keys in firebase*/
 };
 
 initializeApp(firebaseConfig);
